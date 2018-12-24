a0637f06c7fda69fe24fa4ab2a68970358f47b92
谢晟 <40785359+17702513221@users.noreply.github.com>
2018-12-24 17:21:20 +0800

    更新

--- AI_work/yolov3-cpp/start.sh
+++ AI_work/yolov3-cpp/start.sh
@@ -5,4 +5,3 @@ cd build/intel64/Release/
 #./object_detection_demo_yolov3_async -i cam -m /home/xs/tensorflow_tools/tensorflow-yolo-v3/yolo_v3.xml -d CPU 
 ./object_detection_demo_yolov3_async -i cam -m /home/xs/tensorflow_tools/tensorflow-yolo-v3/yolo_v3.xml -d CPU 2>/dev/null | ffmpeg -f rawvideo -pixel_format bgr24 -video_size vga -i - http://localhost:8090/fac.ffm
 #640x480 vga
-#1080 x 720 笔记本
